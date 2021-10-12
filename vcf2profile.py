0f9eff90b6e7d5309406ce427f7fa6186057a663
mjsull <mjsull@gmail.com>
2021-10-12 23:09:40 +1000

    Update vcf2profile.py

--- vcf2profile.py
+++ vcf2profile.py
@@ -1,6 +1,19 @@
+
+
+if len(sys.argv) < 3:
+    print('''
+vcf2profile.py
+Turn a VCF file into a profile for use with grapetree.
+
+USAGE: python vcf2profile.py <input.vcf> <output.profile>
+
+''')
+
+
 import sys
 outlist = []
 newheader = ["GENOME"]
+
 with open(sys.argv[1]) as f, open(sys.argv[2], 'w') as o:
     for line in f:
         if line.startswith("##"):
@@ -11,6 +24,8 @@ with open(sys.argv[1]) as f, open(sys.argv[2], 'w') as o:
                 outlist.append(i)
         else:
             splitline = line.split()
+            if splitline[6] != "PASS":
+                continue
             newheader.append("LOCUS_" + splitline[1])
             for num, i in enumerate(splitline[9:]):
                 if i == "0":
